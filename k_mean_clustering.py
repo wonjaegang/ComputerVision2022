3c9fbf58fbbb68a8b72a678fe016ff3a68f08edb
Wonjaegang <robinsam@naver.com>
2022-05-28 05:04:53 +0900

    Classify the code
    - KMeanCluster 클래스를 생성해 함수들을 메서드로 만들어주었다.

--- k_mean_clustering.py
+++ k_mean_clustering.py
@@ -3,58 +3,105 @@ import numpy as np
 import random
 
 
-def k_mean_clustering(data, k, error_threshold):
-    def average(mean_before, n, a_n1):
-        return (mean_before * n + a_n1) / (n + 1)
+class KMeanClustering:
+    def __init__(self, data, error_threshold=0.01, k_max=10, repetition=10):
+        self.data = data
+        self.error_threshold = error_threshold
+        self.k_max = k_max
+        self.repetition = repetition
 
-    data_range = [[min(data[:, i]), max(data[:, i])] for i in range(data.shape[1])]
-    cluster_location = np.array([[random.uniform(*min_max) for min_max in data_range] for _ in range(k)])
+        self.clustered_points = {}
+        self.cluster_location = {}
+        self.inertia_value = {}
+        self.elbow_k = None
 
-    while True:
-        clustered_point = [[] for _ in range(k)]
-        mean_error = 0
+    def k_mean_clustering_once(self, k):
+        def average(mean_before, n, a_n1):
+            return (mean_before * n + a_n1) / (n + 1)
 
-        # Divide the points into clusters
-        for point in data:
-            norm_array = [[np.linalg.norm(point - cluster, ord=2), i] for i, cluster in enumerate(cluster_location)]
-            nearest_cluster = sorted(norm_array, key=lambda x: x[0])[0][1]
-            clustered_point[nearest_cluster].append(point)
+        data_range = [[min(self.data[:, i]), max(self.data[:, i])] for i in range(self.data.shape[1])]
+        cluster_location = np.array([[random.uniform(*min_max) for min_max in data_range] for _ in range(k)])
 
-        # Update the location of clusters
-        for index in range(k):
-            if clustered_point[index]:
-                new_location = np.array([np.mean(np.array(clustered_point[index])[:, 0]),
-                                         np.mean(np.array(clustered_point[index])[:, 1])])
-            else:
-                new_location = np.array([random.uniform(*min_max) for min_max in data_range])
-            error = np.linalg.norm(new_location - cluster_location[index]) / np.linalg.norm(new_location)
-            mean_error = average(mean_error, index, error)
-            cluster_location[index] = new_location
+        while True:
+            clustered_point = [[] for _ in range(k)]
+            mean_error = 0
 
-        # Evaluate the location of clusters
-        if mean_error < error_threshold:
-            final_clustered_point = clustered_point
-            break
+            # Divide the points into clusters
+            for point in self.data:
+                norm_array = [[np.linalg.norm(point - cluster, ord=2), i] for i, cluster in enumerate(cluster_location)]
+                nearest_cluster = sorted(norm_array, key=lambda x: x[0])[0][1]
+                clustered_point[nearest_cluster].append(point)
 
-    inertia_value = sum([sum([np.linalg.norm(point - cluster, ord=2)
-                              for point in final_clustered_point[i]]) for i, cluster in enumerate(cluster_location)])
+            # Update the location of clusters
+            for index in range(k):
+                if clustered_point[index]:
+                    new_location = np.array([np.mean(np.array(clustered_point[index])[:, 0]),
+                                             np.mean(np.array(clustered_point[index])[:, 1])])
+                else:
+                    new_location = np.array([random.uniform(*min_max) for min_max in data_range])
+                error = np.linalg.norm(new_location - cluster_location[index]) / np.linalg.norm(new_location)
+                mean_error = average(mean_error, index, error)
+                cluster_location[index] = new_location
 
-    return final_clustered_point, cluster_location, inertia_value
+            # Evaluate the location of clusters
+            if mean_error < self.error_threshold:
+                final_clustered_point = clustered_point
+                break
 
+        inertia_value = sum([sum([np.linalg.norm(point - cluster, ord=2)
+                                  for point in final_clustered_point[i]]) for i, cluster in
+                             enumerate(cluster_location)])
 
-def find_elbow_k(array):
-    derivative_array = [x2 - x1 for x1, x2 in zip(array[:-1], array[1:])]
-    elbow_gradient = (derivative_array[0] + derivative_array[-1]) / 2
-    for i, gradient in enumerate(derivative_array):
-        if gradient > elbow_gradient:
-            return (i + 1) + 1
+        return final_clustered_point, cluster_location, inertia_value
+
+    def repeat_K_mean_clustering(self, k):
+        clustered_points = None
+        cluster_location = None
+        inertia_value = float('inf')
+        for _ in range(self.repetition):
+            clustering_result = self.k_mean_clustering_once(k)
+            if clustering_result[2] < inertia_value:
+                clustered_points = clustering_result[0]
+                cluster_location = clustering_result[1]
+                inertia_value = clustering_result[2]
+        return clustered_points, cluster_location, inertia_value
+
+    def find_elbow_k(self):
+        inertia_value_list = list(self.inertia_value.values())
+        d_inertia_value = [x2 - x1 for x1, x2 in zip(inertia_value_list[:-1], inertia_value_list[1:])]
+        elbow_gradient = (d_inertia_value[0] + d_inertia_value[-1]) / 2
+        for i, gradient in enumerate(d_inertia_value):
+            if gradient > elbow_gradient:
+                return (i + 1) + 1
+
+    def plot_clustered_result(self):
+        plt.figure()
+        plt.plot(self.inertia_value.keys(), self.inertia_value.values(), 'r-o')
+        plt.plot(self.elbow_k, self.inertia_value[self.elbow_k], 'b-o')
+
+        plt.figure()
+        for i, points in enumerate(self.clustered_points[self.elbow_k]):
+            plt.scatter(*np.array(points).T,
+                        color=(1 / self.elbow_k * i, 1 - 1 / self.elbow_k * i, 1 / self.elbow_k * i))
+        plt.scatter(*self.cluster_location[self.elbow_k].T, color='r')
+        plt.show()
+
+    def k_mean_clustering(self, plot=True):
+        for k in range(1, self.k_max):
+            clustering_result = self.repeat_K_mean_clustering(k)
+            self.clustered_points[k] = clustering_result[0]
+            self.cluster_location[k] = clustering_result[1]
+            self.inertia_value[k] = clustering_result[2]
+
+        self.elbow_k = self.find_elbow_k()
+
+        if plot:
+            self.plot_clustered_result()
+
+        return self.clustered_points[self.elbow_k]
 
 
 def main():
-    error_threshold = 0.00001
-    k_max = 20
-    random_iterate = 10
-
     data = np.array([[1, 1],
                      [1, 20],
                      [10, 2],
@@ -144,33 +191,8 @@ def main():
                      [4, 1],
                      [4, 4]])
 
-    inertia_value_array = []
-
-    for k in range(1, k_max):
-        clustered_points = None
-        cluster_location = None
-        inertia_value = float('inf')
-        for _ in range(random_iterate):
-            clustering_result = k_mean_clustering(data, k, error_threshold)
-            if clustering_result[2] < inertia_value:
-                clustered_points = clustering_result[0]
-                cluster_location = clustering_result[1]
-                inertia_value = clustering_result[2]
-
-        inertia_value_array.append(inertia_value)
-
-        plt.figure(k)
-        for i, points in enumerate(clustered_points):
-            plt.scatter(*np.array(points).T, color=(1 / k * i, 1 - 1 / k * i, 1 / k * i))
-        plt.scatter(*cluster_location.T, color='r')
-
-    elbow_k = find_elbow_k(inertia_value_array)
-
-    plt.figure(0)
-    plt.plot(range(1, k_max), inertia_value_array, 'r-o')
-    plt.plot(elbow_k, inertia_value_array[elbow_k - 1], 'b-o')
-
-    plt.show()
+    k_mean_clustering = KMeanClustering(data, error_threshold=0.001, k_max=10, repetition=10)
+    k_mean_clustering.k_mean_clustering(plot=True)
 
 
 if __name__ == '__main__':
