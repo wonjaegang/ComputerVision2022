31ac29f55d22be78745aef8eb5f1858c351a895f
Wonjaegang <robinsam@naver.com>
2022-05-29 03:20:39 +0900

    Fix bug
    - plot 에서는 원점이 좌하단이기에 y값에 변화를 주었다.

--- image_segmentation_via_flow.py
+++ image_segmentation_via_flow.py
@@ -8,10 +8,10 @@ def main():
     image_array = get_image_array(img_name_array)
 
     optical_flow = OpticalFlow(image_array,
-                               downscale=2,
+                               downscale=4,
                                neighbor_distance=1,
                                corner_neighbor_distance=1,
-                               corner_threshold=10000)
+                               corner_threshold=1000)
     optical_flow.calculate_optical_flow()
     optical_flow.display_flow()
 
@@ -21,12 +21,14 @@ def main():
         for x in range(data.shape[1]):
             if data[y][x][0] is not None:
                 if np.linalg.norm(data[y][x], ord=2):
-                    a.append([x, y, data[y][x][1], data[y][x][0]])
+                    a.append([x, data.shape[0] - 1 - y, data[y][x][1], -data[y][x][0]])
 
     k_mean_clustering = KMeanClustering(np.array(a), error_threshold=0.001, k_max=20, repetition=5)
     k_mean_clustering.k_mean_clustering(plot=True)
     # k_mean_clustering.repeat_K_mean_clustering(20)
     # k_mean_clustering.plot_clustered_result(20, inertia_value_plot=False)
+
+    plt.show()
 
 
 if __name__ == "__main__":
