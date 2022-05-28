601dc1f79ee2c972081420438351e4c4387efed7
Wonjaegang <robinsam@naver.com>
2022-05-28 14:54:18 +0900

    Classify the code
    - OpticalFlow 클래스를 생성해 함수들을 메서드로 만들어주었다.

--- Optical_Flow.py
+++ Optical_Flow.py
@@ -48,7 +48,6 @@ def window_slice(img, location, neighbor_distance):
 def least_squares_approximation(A, B):
     if np.linalg.det(A.T @ A):
         return np.linalg.inv(A.T @ A) @ A.T @ B
-    # dx = 0 or dy = 0 일때를 따로 고려하자.
     else:
         return np.array([[0], [0]])
 
@@ -72,87 +71,95 @@ def corner_Harris(img, neighbor_distance, threshold):
     return corner_location
 
 
-def optical_flow(img_array, neighbor_distance, corner_setting):
-    flow = np.full((len(img_array) - 1, img_array[0].shape[0], img_array[0].shape[1], 2), None)
+class OpticalFlow:
+    def __init__(self, img_array,
+                 downscale=4, neighbor_distance=1,
+                 corner_neighbor_distance=1, corner_threshold=5000):
+        self.image_array = list(map(lambda x: np.array(down_scale(x, downscale)), img_array))
+        self.neighbor_distance = neighbor_distance
+        self.corner_neighbor_distance = corner_neighbor_distance
+        self.corner_threshold = corner_threshold
 
-    grayscale_image_next = get_grayscale(img_array[-1])
+        self.flow = np.full((len(self.image_array) - 1, self.image_array[0].shape[0], self.image_array[0].shape[1], 2),
+                            None)
 
-    for index, image in enumerate(img_array[-2::-1]):
-        # Gray Scaling & Add Padding
-        grayscale_image = get_grayscale(image)
-        yx_derivative_image = get_derivative(grayscale_image)
-        t_derivative_image = grayscale_image_next - grayscale_image
+    def calculate_optical_flow(self):
+        grayscale_image_next = get_grayscale(self.image_array[-1])
 
-        # # For each pixel
-        # for y in range(grayscale_image.shape[0]):
-        #     for x in range(grayscale_image.shape[1]):
+        for index, image in enumerate(self.image_array[-2::-1]):
+            # Gray Scaling & Add Padding
+            grayscale_image = get_grayscale(image)
+            yx_derivative_image = get_derivative(grayscale_image)
+            t_derivative_image = grayscale_image_next - grayscale_image
 
-        # For each corner
-        corner = corner_Harris(grayscale_image, *corner_setting)
-        for y, x in corner:
-            # Window Slicing & Calculate Derivative
-            window_yx_derivative = window_slice(yx_derivative_image, [y, x], neighbor_distance)
-            window_t_derivative = window_slice(t_derivative_image, [y, x], neighbor_distance)
+            # # For each pixel
+            # for y in range(grayscale_image.shape[0]):
+            #     for x in range(grayscale_image.shape[1]):
 
-            # Approximating for Optimized result
-            vector_dy_dx = window_yx_derivative.reshape((2 * neighbor_distance + 1) ** 2, 2)
-            vector_dt = window_t_derivative.reshape((2 * neighbor_distance + 1) ** 2, 1)
-            optimized_v = least_squares_approximation(vector_dy_dx, -vector_dt)
+            # For each corner
+            corner = corner_Harris(grayscale_image, self.corner_neighbor_distance, self.corner_threshold)
+            for y, x in corner:
+                # Window Slicing & Calculate Derivative
+                window_yx_derivative = window_slice(yx_derivative_image, [y, x], self.neighbor_distance)
+                window_t_derivative = window_slice(t_derivative_image, [y, x], self.neighbor_distance)
 
-            # Save optimized (v, u) at flow matrix
-            flow[-index][y][x] = optimized_v.reshape(2)
+                # Approximating for Optimized result
+                window_size = (2 * self.neighbor_distance + 1) ** 2
+                vector_dy_dx = window_yx_derivative.reshape(window_size, 2)
+                vector_dt = window_t_derivative.reshape(window_size, 1)
+                optimized_v = least_squares_approximation(vector_dy_dx, -vector_dt)
 
-        # Save current pixel values for next loop
-        grayscale_image_next = grayscale_image
-        print("Calculating Optical Flow - (%d/%d)Frame" % (index + 2, len(img_array)))
+                # Save optimized (v, u) at flow matrix
+                self.flow[-index][y][x] = optimized_v.reshape(2)
 
-    return flow
+            # Save current pixel values for next loop
+            grayscale_image_next = grayscale_image
+            print("Calculating Optical Flow - (%d/%d)Frame" % (index + 2, len(self.image_array)))
+
+        return self.flow
+
+    def display_flow(self):
+        plt.style.use('default')
+        fig, ax = plt.subplots()
+        plt.axis([0, self.flow.shape[2], 0, self.flow.shape[1]])
+        for frame_num in range(self.flow.shape[0]):
+            for y in range(self.flow.shape[1]):
+                for x in range(self.flow.shape[2]):
+                    if self.flow[frame_num][y][x][0] is not None:
+                        ax.add_patch(
+                            patches.Arrow(
+                                x, self.flow.shape[1] - 1 - y,
+                                self.flow[frame_num][y][x][1], -self.flow[frame_num][y][x][0],
+                                width=0.3,
+                                edgecolor='deeppink',
+                                facecolor='white'
+                            ))
+        plt.show()
+        return 0
 
 
-def display_flow(flow_array):
-    plt.style.use('default')
-    fig, ax = plt.subplots()
-    plt.axis([0, flow_array.shape[2], 0, flow_array.shape[1]])
-    for index in range(flow_array.shape[0]):
-        for y in range(flow_array.shape[1]):
-            for x in range(flow_array.shape[2]):
-                if flow_array[index][y][x][0] is not None:
-                    ax.add_patch(
-                        patches.Arrow(
-                            x, flow_array.shape[1] - 1 - y,
-                            flow_array[index][y][x][1], -flow_array[index][y][x][0],
-                            width=0.3,
-                            edgecolor='deeppink',
-                            facecolor='white'
-                        ))
-    plt.show()
-    return 0
-
-
-def get_image_name(video_name, frame):
+def get_image_name_from_video(video_name, frame):
     return ['%s/%dFrame/%s %03d.jpg' % (video_name, frame, video_name, x + 1) for x in range(frame)]
 
 
-def get_image_array(downscale, img_name_array):
-    return [np.array(down_scale(Image.open(x), downscale)) for x in img_name_array]
+def get_image_array(img_name_array):
+    return [Image.open(x) for x in img_name_array]
 
 
 def main():
-    # 사용자 설정 값
     video_name = 'Paris'
     frame = 100
-    downscale = 4
-    neighbor_distance = 1
-    corner_neighbor_distance = 1
-    corner_threshold = 5000
-    corner_setting = [corner_neighbor_distance, corner_threshold]
-
+    # img_name_array = get_image_name_from_video(video_name, frame)
     img_name_array = ['Paris/200Frame/Paris 180.jpg', 'Paris/200Frame/Paris 181.jpg']
-    # img_name_array = get_image_name(video_name, frame)
-    image_array = get_image_array(downscale, img_name_array)
+    image_array = get_image_array(img_name_array)
 
-    flow_array = optical_flow(image_array, neighbor_distance, corner_setting)
-    display_flow(flow_array)
+    optical_flow = OpticalFlow(image_array,
+                               downscale=4,
+                               neighbor_distance=1,
+                               corner_neighbor_distance=1,
+                               corner_threshold=5000)
+    optical_flow.calculate_optical_flow()
+    optical_flow.display_flow()
 
 
 if __name__ == '__main__':
