472daddd2d0f84aac1659c1d218169975702e9af
Wonjaegang <robinsam@naver.com>
2022-06-01 00:34:00 +0900

    Add code for dividing means
    - 이동된 평균값들을 분류하는 코드를 구현하였다.

--- mean_shift_clustering.py
+++ mean_shift_clustering.py
@@ -14,6 +14,7 @@ class MeanShiftClustering:
 
         self.memoization_dict = {}
         self.shifted_mean = np.zeros(data.shape)
+        self.clustered_index = []
 
     def get_data_range(self):
         return [[min(self.data[:, i]), max(self.data[:, i])] for i in range(self.data_dimension)]
@@ -40,7 +41,6 @@ class MeanShiftClustering:
         mean_vector = point
         while True:
             updated_mean_vector = self.calculate_neighbor_mean(mean_vector)
-            print(mean_vector, updated_mean_vector)
             error = np.linalg.norm(mean_vector - updated_mean_vector, ord=2)
             mean_vector = updated_mean_vector
             if error < self.error_threshold:
@@ -48,34 +48,91 @@ class MeanShiftClustering:
 
     def plot_clustered_result(self):
         plt.figure()
+        plt.gca().invert_yaxis()
         plt.scatter(*np.array(self.normalized_data).T[:2, :],
                     color='b',
-                    s=50)
+                    s=10)
         plt.scatter(*np.array(self.shifted_mean).T[:2, :],
                     color='r',
                     s=5)
+
+        plt.figure()
         plt.gca().invert_yaxis()
+        k = len(self.clustered_index)
+        for cluster_i, indexes in enumerate(self.clustered_index):
+            points = np.array([self.data[index] for index in indexes])
+            plt.scatter(*points.T[:2, :],
+                        color=(1 / k * cluster_i, 1 - 1 / k * cluster_i, 1 / k * cluster_i),
+                        s=10)
 
     def mean_shift_clustering(self):
         for point_i, point in enumerate(self.normalized_data):
             self.shifted_mean[point_i] = self.shift_mean(point)
             print("Shifting Mean - (%d/%d)data" % (point_i + 1, self.data.shape[0]))
 
-        self.plot_clustered_result()
-        plt.show()
-
-        # criterion_dict = {}
-        # for mean_vector in self.shifted_mean:
-        #     for criterion in criterion_dict:
-        #         if np.linalg.norm(mean_vector - criterion, ord=2) < self.error_threshold:
-        #             criterion_dict[criterion]
+        shifted_mean_with_index = [[i, mean_vector] for i, mean_vector in enumerate(self.shifted_mean)]
+        while shifted_mean_with_index:
+            indexes = []
+            for index, mean_vector in shifted_mean_with_index:
+                print(index, np.linalg.norm(mean_vector - shifted_mean_with_index[-1][1], ord=2))
+                if np.linalg.norm(mean_vector - shifted_mean_with_index[-1][1], ord=2) < self.error_threshold:
+                    indexes.append(index)
+            shifted_mean_with_index = [x for x in shifted_mean_with_index if x[0] not in indexes]
+            print(indexes)
+            self.clustered_index.append(indexes)
 
 
 def main():
-    data = np.array([[x, y] for x, y in zip(list(range(10)), list(range(10)))])
-    print(data)
-    mean_shift_clustering = MeanShiftClustering(data, error_threshold=0.01, window_radius=0.1)
+    data = np.array([[1, 0],
+                     [1, 1],
+                     [1, 2],
+                     [1, 3],
+                     [1, 4],
+                     [1, 5],
+                     [1, 6],
+                     [1, 7],
+                     [1, 8],
+                     [1, 9],
+                     [1, 10],
+                     [2, 0],
+                     [2, 1],
+                     [2, 2],
+                     [2, 3],
+                     [2, 4],
+                     [2, 5],
+                     [2, 6],
+                     [2, 7],
+                     [2, 8],
+                     [2, 9],
+                     [2, 10],
+                     [11, 10],
+                     [11, 11],
+                     [11, 12],
+                     [11, 13],
+                     [11, 14],
+                     [11, 15],
+                     [11, 16],
+                     [11, 17],
+                     [11, 18],
+                     [11, 19],
+                     [11, 20],
+                     [12, 10],
+                     [12, 11],
+                     [12, 12],
+                     [12, 13],
+                     [12, 14],
+                     [12, 15],
+                     [12, 16],
+                     [12, 17],
+                     [12, 18],
+                     [12, 19],
+                     [12, 20]])
+
+    mean_shift_clustering = MeanShiftClustering(data, error_threshold=0.001, window_radius=0.3)
     mean_shift_clustering.mean_shift_clustering()
+    mean_shift_clustering.plot_clustered_result()
+
+    plt.show()
 
 
 if __name__ == '__main__':
